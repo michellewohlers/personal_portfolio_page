2ab3dc356a4929301c4471c5c19924ce3bff6aaf
Michelle Wohlers <michelle-wohlers@web.de>
2024-03-18 11:40:12 +0000

    Created Webpage and Tablet Version

---
+++ resources/index.js
@@ -0,0 +1,9 @@
+const projectimg = document.getElementsByClassName("projectimg");
+
+function zoom (projectimg) {
+    projectimg.style.transform = 'scale(1.5)';
+};
+
+function rezoom (projectimg) {
+    projectimg.style.transform = 'scale(1)';
+}
